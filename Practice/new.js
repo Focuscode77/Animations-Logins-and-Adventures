bc462f9a8bd52927057dc8ce351d37355353c9f0
Joshua Coleman <procoleman77@outlook.com>
2020-04-18 12:27:01 -0400

    Added javascript content

--- Practice/new.js
+++ Practice/new.js
@@ -24,27 +24,61 @@
 
 
 
-var pointPeople = function (name, age, occupation) {
-    this.name = name;
+// var pointPeople = function (name, age, occupation) {
+//     this.name = name;
 
-    this.age = age;
+//     this.age = age;
 
-    this.occupation = occupation;
+//     this.occupation = occupation;
+// }
+
+// console.log(pointPeople);
+
+
+// var coWorker = new pointPeople("James", 25, "Chef");
+
+// var coWorkerOne = new pointPeople("Peter", 30, "Software enignneer");
+// var coWorkerTwo = new pointPeople("Josh", 29, "Ice Maker");
+// var coWorkerThree = new pointPeople("Daxx", 50, "Police Oficer");
+
+// // Testing Code
+
+
+// console.log(coWorker);
+// console.log(coWorkerOne);
+// console.log(coWorkerThree);
+// document.write(coWorkerTwo.name);
+
+// PRATCING IF STATEMENTS AND Conditions
+// If statement 
+var a = 10;
+if (a < 4) {
+    console.log("A is greater");
+
+
+} else {
+    console.log("A is not greater");
+
 }
 
-console.log(pointPeople);
+
+// Conditional statement
+var pratice = 5;
+
+switch (pratice) {
+    case 5:
+        document.write("case 1 is true");
+        break;
+    default:
+        document.write("case not meet!")
 
 
-var coWorker = new pointPeople("James", 25, "Chef");
 
-var coWorkerOne = new pointPeople("Peter", 30, "Software enignneer");
-var coWorkerTwo = new pointPeople("Josh", 29, "Ice Maker");
-var coWorkerThree = new pointPeople("Daxx", 50, "Police Oficer");
+}
 
-// Testing Code
+var myArray = [1, 2];
+console.log(myArray[0]);
 
+var myObject = { age: "27", name: "Josh" };
+console.log(myObject.name)
 
-console.log(coWorker);
-console.log(coWorkerOne);
-console.log(coWorkerThree);
-document.write(coWorkerTwo.name);
